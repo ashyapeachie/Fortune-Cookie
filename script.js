2770c5d6aa400318c373e958ab43bcf9176e826e
ashyapeachie <128183143+ashyapeachie@users.noreply.github.com>
2026-07-11 18:28:03 -0400

    saving progress from fixing phases 1, 2, and 3

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const fortunes = [
-    "The best is yet to come.",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
+    "You are full of love",
+    "You are rich in all areas of life",
+    "You are a magnet for miracles, success, and positivity!",
+    "There is always more than enough money",
+    "The money you spend comes back x3!",
+    "The desire has already manifested",
+    "More money, more power, more abundance",
+    "Your talents will be recognized and rewarded",
+    "Savings Account: $10,000"
 ];
 
 const button = document.getElementById("fortuneButton");
