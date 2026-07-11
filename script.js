23d875ca5f9a3dc04bae80478f41df64d53bba5a
ashyapeachie <128183143+ashyapeachie@users.noreply.github.com>
2026-07-11 01:40:30 -0400

    update to HTML, CSS, and JS files
    built HTML structure, added styling to page, and started the array to make program work

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const fortunes = [
-
+    "The best is yet to come.",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
 ];
 
-const button 
+const button = document.getElementById("fortuneButton");
+const fortuneText = document.getElementById("fortuneText");
+
+button.addEventListener("click", () => {
+    const randomIndex = Math.floor(
+        Math.random() * fortunes.length
+    );
+
+    fortuneText.textContent = fortunes[randomIndex];
+});
